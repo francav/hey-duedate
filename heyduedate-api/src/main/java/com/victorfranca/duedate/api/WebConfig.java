7748082828c71ce2a733b90e698fd427b5ce335f
Franca, Victor <victor.franca@fmr.com>
2022-03-16 16:22:16 +0000

    #27 fixing issue with CORS begind proxy

--- heyduedate-api/src/main/java/com/victorfranca/duedate/api/WebConfig.java
+++ heyduedate-api/src/main/java/com/victorfranca/duedate/api/WebConfig.java
@@ -13,7 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+				.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+						"Access-Control-Request-Headers")
+				.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
 				.allowCredentials(true).maxAge(3600);
 	}
 
