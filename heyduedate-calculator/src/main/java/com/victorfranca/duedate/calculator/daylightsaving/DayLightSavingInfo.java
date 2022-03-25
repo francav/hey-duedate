f97b18dceebc4e48f1e36ffb24f680553034830d
Victor Franca <victor@wkspower.com>
2022-03-25 19:46:29 +0000

    changing license

--- heyduedate-calculator/src/main/java/com/victorfranca/duedate/calculator/daylightsaving/DayLightSavingInfo.java
+++ heyduedate-calculator/src/main/java/com/victorfranca/duedate/calculator/daylightsaving/DayLightSavingInfo.java
@@ -2,10 +2,10 @@
  * Copyright WKS Power Limited under one or more contributor license agreements. 
  * See the LICENSE file distributed with this work for additional information regarding 
  * copyright ownership. WKS Power licenses this file to you under the 
- * GNU General Public License v3.0; you may not use this file except in compliance with 
+ * GNU AFFERO GENERAL PUBLIC LICENSE v3.0; you may not use this file except in compliance with 
  * the License. You may obtain a copy of the License at
  *
- *     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *     https://www.gnu.org/licenses/agpl-3.0.en.html
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
